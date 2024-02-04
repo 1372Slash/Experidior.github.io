955f035b299424e6d0291d83b1434e2b45c93158
1372Slash <133662518+1372Slash@users.noreply.github.com>
2024-02-04 21:00:02 +0800

    Add : hamburger menu (not final)

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function goBack(){
     window.history.back();
 }
+function menubars() {
+    var x = document.getElementById("linked");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
